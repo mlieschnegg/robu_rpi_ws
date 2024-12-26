6a25da824af7447df94431f4c1a2484e784a40c7
mlieschnegg <li@htl-kaindorf.at>
2024-12-26 23:25:49 +0100

    updatet autostart skript

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -1,14 +1,18 @@
-set -e #Beendet das Skript bei Fehlern
+#set -e #Beendet das Skript bei Fehlern
 
 export ROBU_RPI_WS=/home/robu/work/robu_rpi_ws
 export DISPLAY=:0
 
+cd $ROBU_RPI_WS
+git pull
+cd
+
 # Prüfen, ob der Prozess "screen" existiert
-if pgrep screen > /dev/null; then
-    killall screen
-fi
-killall -9 /usr/bin/python3
+#if pgrep screen > /dev/null; then
+#    killall screen
+#fi
+#killall -9 /usr/bin/python3
 
 source /opt/ros/humble/setup.bash
 
-/usr/bin/python3 ROBU_RPI_WS/autostart/autostart.py
+/usr/bin/python3 $ROBU_RPI_WS/autostart/autostart.py
