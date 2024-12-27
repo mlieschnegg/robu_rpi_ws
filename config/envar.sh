d1d079e380a04f574acbd352f909f0c435272934
mlieschnegg <li@htl-kaindorf.at>
2024-12-27 22:26:42 +0100

    envar update

--- config/envar.sh
+++ config/envar.sh
@@ -3,7 +3,7 @@ export OPENCR_MODEL=burger
 export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-02
 export GST_PLUGIN_PATH=/usr/local/lib/aarch64-linux-gnu/
-export FASTRTPS_DEFAULT_PROFILES_FILE=/home/robu/work/robu_rpi_ws/config/fastdds.xml
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/robu/work/.robu/config/fastdds.xml
 
 source /opt/ros/humble/setup.bash
 source /home/robu/work/turtlebot3_ws/install/setup.bash
