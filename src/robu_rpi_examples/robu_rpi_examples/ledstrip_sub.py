63166b0590a8079a8b2359b1c7be44ed5d5de20e
mlieschnegg <li@htl-kaindorf.at>
2025-01-22 21:04:13 +0100

    ledstrip subscriber added user led

--- src/robu_rpi_examples/robu_rpi_examples/ledstrip_sub.py
+++ src/robu_rpi_examples/robu_rpi_examples/ledstrip_sub.py
@@ -23,6 +23,35 @@ import rclpy
 
 #ros2 service call /camera_bl leddisplay_interfaces/srv/LEDCamera "{camera: left, brightness_percent: 50}"
 
+
+
+NAME_DICT = {
+    "li":{"katnr": 0, "vorname": "Michael"},
+    "ambtin20": {"katnr": 1, "vorname": "Timo", "color": [255,0,0,0]},
+    "baylun20": {"katnr": 2, "vorname": "Lukas", "color": [0,255,0,0]},
+    "forman21": {"katnr": 3, "vorname": "Marco", "color": [0,0,255,0]},
+    "dorjon21": {"katnr": 4, "vorname": "Jonas", "color": [255,0,0,0]},
+    "engsan21": {"katnr": 5, "vorname": "Samuel", "color": [0,255,0,0]},
+    "fasmin21": {"katnr": 6, "vorname": "Michael", "color": [0,0,255,0]},
+    "ferdon21": {"katnr": 7, "vorname": "Dominik", "color": [255,0,0,0]},
+    "finman21": {"katnr": 8, "vorname": "Maximilian", "color": [0,255,0,0]},
+    "frisan21": {"katnr": 9, "vorname": "Sarah", "color": [0,0,255,0]},
+    "hadman21": {"katnr": 10,"vorname": "Matthias", "color": [255,0,0,0]},
+    "hauran21": {"katnr": 11,"vorname": "Rafael", "color": [0,255,0,0]},
+    "husfln21": {"katnr": 12,"vorname": "Florentina", "color": [0,0,255,0]},
+    "hutaln21": {"katnr": 13,"vorname": "Alexander", "color": [255,0,0,0]},
+    "kiealn20": {"katnr": 14,"vorname": "Alexander", "color": [0,255,0,0]},
+    "kocman21": {"katnr": 15,"vorname": "Maximilian", "color": [0,0,255,0]},
+    "krealn21": {"katnr": 16,"vorname": "Alexander", "color": [255,0,0,0]},
+    "magman21": {"katnr": 17,"vorname": "Matthias", "color": [0,255,0,0]},
+    "orophn21": {"katnr": 18,"vorname": "Philip", "color": [0,0,255,0]},
+    "seuchn20": {"katnr": 19,"vorname": "Christoph", "color": [255,0,0,0]},
+    "soelun21": {"katnr": 20,"vorname": "Lukas", "color": [0,255,0,0]},
+    "swenin21": {"katnr": 21,"vorname": "Nico", "color": [0,0,255,0]},
+    "taxpan20": {"katnr": 22,"vorname": "Patrick", "color": [255,0,0,0]},
+    "weijun21": {"katnr": 23,"vorname": "Julian", "color": [0,255,0,0]}
+}
+
 def new_ledstrip(numleds:int) -> Adafruit_NeoPixel:
     if not is_raspberry_pi():
         return None
@@ -56,6 +85,12 @@ class LEDDisplay(Node):
 
         #Format: [lednr1 r1, g1, b1, w1, lednr2, r2, g2, b2, w2]
         self.create_subscription(Int8MultiArray, "team1", self._team1_callback, 10)
+        self.create_subscription(Int8MultiArray, "team2", self._team2_callback, 10)
+        self.create_subscription(Int8MultiArray, "team3", self._team3_callback, 10)
+        self.create_subscription(Int8MultiArray, "team4", self._team4_callback, 10)
+        self.create_subscription(Int8MultiArray, "team5", self._team5_callback, 10)
+        self.create_subscription(Int8MultiArray, "team6", self._team6_callback, 10)
+        self.create_subscription(String, "user", self._user_callback, 10)
 
         self._team_leds_config = [
             {"pos": list(range(self.TEAM_LED_OFFSET+self.TEAM_LED_NUM*0, self.TEAM_LED_OFFSET+self.TEAM_LED_NUM*1)), "default-color": [0,0,0,255]},
@@ -87,11 +122,52 @@ class LEDDisplay(Node):
 
 
     def _team1_callback(self, msg:Int8MultiArray):
-        #length: lednr, red, green, blue, white
+        #msg.data Format: lednr0, red0, green0, blue0, white0, lednr1, red1, green1, blue1, white1, ...
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 0)
+        
+    def _team2_callback(self, msg:Int8MultiArray):
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 1)
+
+    def _team3_callback(self, msg:Int8MultiArray):
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 2)
+
+    def _team4_callback(self, msg:Int8MultiArray):
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 3)
+
+    def _team5_callback(self, msg:Int8MultiArray):
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 4)
+
+    def _team6_callback(self, msg:Int8MultiArray):
+        led_vals = list(msg.data)
+        self._update_team(led_vals, 5)
+
+    def _user_callback(self, msg:String):
+        #Format = "benutzername" e.q li
+        try:
+            
+            lednr = NAME_DICT[msg.data]["katnr"]
+            color = NAME_DICT[msg.data]["color"]
+            if (self._led_values[lednr]):
+                self._led_values[lednr] = [0, 0, 0, 0]
+                self.get_logger().info(f"{NAME_DICT[msg.data]["vorname"]} -> LED wurde DEAKTIVIERT!")
+            else:
+                self._led_values[lednr] = color
+                self.get_logger().info(f"{NAME_DICT[msg.data]["vorname"]} -> LED wurde AKTIVIERT!")
+            self._hw_led_strip_set_color()
+        except Exception as warn:
+            self.get_logger().warn(warn)
+    
+
+    def _update_team(self, led_vals:list, teamnr:int=0):
         DATA_LEN = 5
-        for i in range(0, len(msg.data), DATA_LEN):
-            led_nr, red_val, green_val, blue_val, white_val = msg.data[i:i+DATA_LEN]
-            self._led_values[led_nr] = [red_val, green_val, blue_val, white_val]
+        for i in range(0, len(led_vals), DATA_LEN):
+            led_nr, red_val, green_val, blue_val, white_val = led_vals[i:i+DATA_LEN]
+            self._led_values[self.TEAM_LED_OFFSET + teamnr*self.TEAM_LED_NUM+led_nr] = [red_val, green_val, blue_val, white_val]
 
         self._hw_led_strip_set_color()
 
