5b44334393cee7ddf652e733215e2c3e96a34eee
mlieschnegg <li@htl-kaindorf.at>
2025-09-01 10:25:25 +0200

    setup for machine learning

---
+++ robu_setup/machine_learning_setup.sh
@@ -0,0 +1,55 @@
+
+
+# mkdir -p ~/miniconda3
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+# rm ~/miniconda3/miniconda.sh
+
+# source ~/miniconda3/bin/activate
+
+# conda init --all
+
+
+# #Tests
+# conda list
+# conda --version
+
+
+# conda create --name „robu_conda“ python=3.10
+ 
+# conda activate „robu_conda“
+ 
+# conda update -n base -c defaults conda
+ 
+# conda install numpy
+ 
+# conda install pillow
+ 
+# pip install opencv-python
+ 
+# pip install opencv-contrib-python
+ 
+# conda install pyqt
+ 
+# conda install matplotlib
+ 
+# pip install jupyterlab
+ 
+# jupyter lab
+
+
+pip install jupyterlab
+
+#pip install notebook
+pip install --upgrade pip
+pip install numpy
+pip install pillow
+#pip install opencv-python==4.2.0
+pip install opencv-contrib-python
+pip install pyqt5
+pip install matplotlib
+pip install keras
+pip install scipy
+pip install scikit-learn
+
+# jupyter lab
