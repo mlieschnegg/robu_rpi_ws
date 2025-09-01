98f494e62728963d47ada4acfe2de87f0d3b2aef
mlieschnegg <li@htl-kaindorf.at>
2025-09-01 09:19:42 +0200

    new envars...

--- config/envar.sh
+++ config/envar.sh
@@ -3,7 +3,12 @@ export OPENCR_MODEL=burger
 export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-02
 export GST_PLUGIN_PATH=/usr/local/lib/aarch64-linux-gnu/
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=/home/robu/work/.robu/config/fastdds.xml
+
+#export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+#export CYCLONEDDS_URI=/home/robu/work/.robu/config/cyclonedds.xml
+#export CYCLONEDDS_NETWORK_INTERFACE_ADDRESS=192.168.0.234
 
 source /opt/ros/humble/setup.bash
 source /home/robu/work/turtlebot3_ws/install/setup.bash
