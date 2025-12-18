00141554b2e2428aadc6feec89af427a4fa71c7b
techtitans-htlk <rcj@htl-kaindorf.at>
2025-12-19 00:28:45 +0100

    update of service files

---
+++ config/service/is_robuboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../../install/setup.bash"
+
+/usr/bin/python3 - <<'PY'
+from robuboard.rpi.utils import is_robuboard
+raise SystemExit(0 if is_robuboard() else 1)
+PY
