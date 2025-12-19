d04a14124f7825d78b0799997c2b526d3e20c38a
techtitans-htlk <rcj@htl-kaindorf.at>
2025-12-19 17:53:31 +0100

    added output to is_robubaord_v1 bash script

--- config/service/is_robuboard.sh
+++ config/service/is_robuboard.sh
@@ -6,5 +6,8 @@ source "$SCRIPT_DIR/../../install/setup.bash"
 
 /usr/bin/python3 - <<'PY'
 from robuboard.rpi.utils import is_robuboard_v1
-raise SystemExit(0 if is_robuboard_v1() else 1)
+
+result = is_robuboard_v1()
+print(f"RobuBoard v1 detected: {result}")
+raise SystemExit(0 if result else 1)
 PY
