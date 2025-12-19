725068642694433a893ca29e248b683fa479832c
techtitans-htlk <rcj@htl-kaindorf.at>
2025-12-19 11:19:39 +0100

    start services only von robuboard v1

--- config/service/is_robuboard.sh
+++ config/service/is_robuboard.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../../install/setup.bash"
 
 /usr/bin/python3 - <<'PY'
-from robuboard.rpi.utils import is_robuboard
+from robuboard.rpi.utils import is_robuboard_v1
 raise SystemExit(0 if is_robuboard() else 1)
 PY
